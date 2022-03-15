8a0bad98d0ad3968bc20d995f95c98b79978396b
duongtai26495 <taiduong.264@gmail.com>
2022-03-15 11:19:19 +0700

    Create sourcebase

---
+++ src/main/java/com/shopdesk/hitech/ShopHiTechApplication.java
@@ -0,0 +1,13 @@
+package com.shopdesk.hitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHiTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHiTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopdesk/hitech/ShopHiTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopdesk.hitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopHiTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
